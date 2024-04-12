916d52270172317fba9b782f1fb925975be5bac7
guilhermBM <166859672+guilhermBM@users.noreply.github.com>
2024-04-12 15:09:19 -0300

    Upgrade de saída de sistema
    Foi implementado a opção de saída do sistema

--- cartorio_ebac.c
+++ cartorio_ebac.c
@@ -147,6 +147,7 @@ int main()
       printf("\t1 - Registrar nomes\n");
 	  printf("\t2 - Consultar nomes\n");
 	  printf("\t3 - Deletar nomes\n\n");
+	  printf("\t4 - Sair do sistema\n\n");
 	  printf("Opção:");//Fim do menu
 	
 	  scanf("%d", &opcao);//Armazenando a escolha do usuário
@@ -165,6 +166,11 @@ int main()
 		
 		case 3: 
 	  	deletar();//chamada de funções
+		break;
+		
+		case 4:
+		printf("Obrigado por usar nosso sistema!");
+		return 0;
 		break;
 		
 		default: 
